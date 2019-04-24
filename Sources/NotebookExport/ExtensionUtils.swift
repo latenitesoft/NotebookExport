793a3ac548882913de03f7fe94d1d853a719734f
Pedro Cuenca <pedro@latenitesoft.com>
2019-04-24 01:10:22 -0700

    * Extend Path with hard links.

--- Sources/NotebookExport/ExtensionUtils.swift
+++ Sources/NotebookExport/ExtensionUtils.swift
@@ -32,6 +32,36 @@ extension Path {
     static func from(_ path: String) -> Path {
         return Path(path) ?? Path.cwd/path
     }
+    
+    /* Copied from symlink implementation at https://github.com/mxcl/Path.swift/blob/master/Sources/Path%2BFileManager.swift */
+    
+    /**
+     Creates a hardlink of this file at `as`.
+     - Note: If `self` does not exist, is **not** an error.
+     */
+    @discardableResult
+    func link(as: Path) throws -> Path {
+        try FileManager.default.linkItem(atPath: string, toPath: `as`.string)
+        return `as`
+    }
+    
+    /**
+     Creates a symlink of this file with the same filename in the `into` directory.
+     - Note: If into does not exist, creates the directory with intermediate directories if necessary.
+     */
+    @discardableResult
+    func link(into dir: Path) throws -> Path {
+        switch dir.kind {
+        case nil, .symlink?:
+            try dir.mkdir(.p)
+            fallthrough
+        case .directory?:
+            return try link(as: dir/basename())
+        case .file?:
+            throw CocoaError.error(.fileWriteFileExists)
+        }
+    }
+
 }
 
 extension String {
