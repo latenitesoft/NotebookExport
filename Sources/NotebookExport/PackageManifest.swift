ac2903267cbf248f1dbbbf290531f7fa23af4db6
Pedro Cuenca <pedro@latenitesoft.com>
2019-05-02 18:00:41 -0700

    * Allow manifest to include executable products.

--- Sources/NotebookExport/PackageManifest.swift
+++ Sources/NotebookExport/PackageManifest.swift
@@ -5,9 +5,36 @@ import Path
 struct PackageManifest {
     var packagePath: Path
     var dependencies: [DependencyDescription]
+    var executableNames: [String]
+
+    // Create initializers so executableNames can be skipped
     
+    init(packagePath: Path, dependencies: [DependencyDescription], executableNames: [String]) {
+        self.packagePath = packagePath
+        self.dependencies = dependencies
+        self.executableNames = executableNames
+    }
+
+    init(packagePath: Path, dependencies: [DependencyDescription]) {
+        self.init(packagePath: packagePath, dependencies: dependencies, executableNames: [])
+    }
+
     var packageName: String { return packagePath.basename() }
     
+    var executableProducts: String {
+        return executableNames.map {
+            ".executable(name: \($0.quoted), targets: [\($0.quoted)])"
+        }
+        .joined(separator: ",\n    ")
+    }
+    
+    var executableTargets: String {
+        return executableNames.map {
+            ".target(name: \($0.quoted), dependencies: [\(packageName.quoted)])"
+        }
+        .joined(separator: ",\n    ")
+    }
+
     var manifest : String {
         let dependencyPackages = (dependencies.map { return $0.spec(relativeTo: packagePath) }).joined(separator: ",\n    ")
         let dependencyNames = (dependencies.map { return "\($0.name.quoted)" }).joined(separator: ", ")
@@ -19,14 +46,14 @@ struct PackageManifest {
         name: "\(packageName)",
         products: [
         .library(name: "\(packageName)", targets: ["\(packageName)"]),
+        \(executableProducts)
         ],
         dependencies: [
         \(dependencyPackages)
         ],
         targets: [
-        .target(
-        name: "\(packageName)",
-        dependencies: [\(dependencyNames)]),
+        .target(name: "\(packageName)", dependencies: [\(dependencyNames)]),
+        \(executableTargets)
         ]
         )
         """
